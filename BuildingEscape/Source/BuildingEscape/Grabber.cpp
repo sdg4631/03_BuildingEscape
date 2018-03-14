57c66661e98d276c3b60c766c53d3754bad6906f
S&K <sdg4631@gmail.com>
2018-03-14 15:44:50 +0900

    Added Grab Release with log output

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -42,6 +42,7 @@ void UGrabber::BeginPlay()
 		UE_LOG(LogTemp, Warning, TEXT("Input component is found."))
 			/// Bind the input axis
 			InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+			InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
 	}
 	else
 	{
@@ -52,6 +53,11 @@ void UGrabber::BeginPlay()
 void UGrabber::Grab() 
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab pressed!"))
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grab released!"))
 }
 
 // Called every frame
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -42,4 +42,7 @@ private:
 	
 	// Ray-cast and grab what's in reach
 	void Grab();
+
+	// Called when grab is released
+	void Release();
 };
